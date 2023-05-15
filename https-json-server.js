17406275b599e80d97bd5f3e45dbe55b2a2c4fe8
cholatis <tok0126@hotmail.com>
2023-05-15 08:43:14 +0700

    Add files via upload
    npm init -y
    npm install json-server
    npm install connect-pause
    npm install path
    npm install fs
    node https-json-server.js

---
+++ https-json-server.js
@@ -0,0 +1,52 @@
+// https-json-server.js
+// Run with: node https-json-server.js
+
+// Generate SSL keys: openssl req -nodes -new -x509 -keyout server.key -out server.cert
+
+const jsonServer = require("json-server");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const pause = require('connect-pause'); // Install: npm install connect-pause
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+server.use(pause(2000));
+server.use(router);
+
+// If using custom routes
+//var routes = JSON.parse(fs.readFileSync('routes.json'));
+//server.use(jsonServer.rewriter(routes));
+
+const keyFile = path.join(__dirname, 'private.key');
+const certFile = path.join(__dirname, 'pttro_com_pack.crt');
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync(keyFile),
+      cert: fs.readFileSync(certFile),
+    },
+    server
+  )
+  .listen(3001, '0.0.0.0', () => {
+    console.log(
+      'Go to https://localhost:3001/'
+    );
+  });
+
